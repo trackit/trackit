d10539a72ac46e277e442ad8f3615c2f8fae8d20
Hugo SCHOCH <schoch.hugo@gmail.com>
2017-10-12 20:07:26 +0200

    Refactor actions ( Add directories )

---
+++ src/actions/gcp/index.js
@@ -0,0 +1,5 @@
+import Pricing from './pricingActions';
+
+export default {
+	Pricing
+};
---
+++ src/actions/gcp/pricingActions.js
@@ -0,0 +1,7 @@
+import Constants from '../../constants';
+
+export default {
+	getPricing: () => ({
+		type: Constants.GCP_GET_PRICING
+	})
+};
