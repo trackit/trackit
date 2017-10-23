9767ad85c4ce06fe8dc30fa35ae0159e215bf42e
Hugo SCHOCH <schoch.hugo@gmail.com>
2017-10-23 16:57:08 +0200

    Add Authorization in headers

--- src/api/misc.js
+++ src/api/misc.js
@@ -1,13 +1,16 @@
 import Config from '../config.js';
 
-export const call = (route, method, body=null) => {
+export const call = (route, method, body=null, token=null) => {
+  let headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  };
+  if (token !== null)
+    headers['Authorization'] = token;
   return fetch(Config.apiUrl + route, {
     method,
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    },
+    headers,
     body: (body !== null ? JSON.stringify(body) : null)
-  }).then(response => ({success: true, ...response }))
+  }).then(response => ({success: true, data: response }))
     .then(error => ({success: false, ...error}));
 };
