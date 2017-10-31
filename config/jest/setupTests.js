f6db4b91d8a038cb74034a08b1e93872cc536944
Hugo SCHOCH <schoch.hugo@gmail.com>
2017-10-31 13:55:48 +0100

    Add mock for LocalStorage

--- config/jest/setupTests.js
+++ config/jest/setupTests.js
@@ -7,3 +7,27 @@ configure({ adapter: new Adapter() });
 global.requestAnimationFrame = (callback) => {
   setTimeout(callback, 0);
 };
+
+let localStorageMock = (function() {
+  let store = {};
+
+  return {
+    getItem: function(key) {
+      return store[key] || null;
+    },
+    setItem: function(key, value) {
+      store[key] = value.toString();
+    },
+    removeItem: function(key) {
+      delete store[key];
+    },
+    clear: function() {
+      store = {};
+    }
+  };
+
+})();
+
+Object.defineProperty(window, 'localStorage', {
+  value: localStorageMock
+});
