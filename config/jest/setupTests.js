57988f176d08d4a5be0c385dda6ea422c2c8130c
Hugo SCHOCH <schoch.hugo@gmail.com>
2017-11-14 11:16:40 +0100

    Add Jest config for Plotly

--- config/jest/setupTests.js
+++ config/jest/setupTests.js
@@ -8,26 +8,30 @@ global.requestAnimationFrame = (callback) => {
   setTimeout(callback, 0);
 };
 
-let localStorageMock = (function() {
-  let store = {};
-
-  return {
-    getItem: function(key) {
-      return store[key] || null;
-    },
-    setItem: function(key, value) {
-      store[key] = value.toString();
-    },
-    removeItem: function(key) {
-      delete store[key];
-    },
-    clear: function() {
-      store = {};
-    }
-  };
-
-})();
-
 Object.defineProperty(window, 'localStorage', {
-  value: localStorageMock
+  value: (function() {
+    let store = {};
+
+    return {
+      getItem: function(key) {
+        return store[key] || null;
+      },
+      setItem: function(key, value) {
+        store[key] = value.toString();
+      },
+      removeItem: function(key) {
+        delete store[key];
+      },
+      clear: function() {
+        store = {};
+      }
+    };
+
+  })()
 });
+
+Object.defineProperty(window, 'Plotly', {
+  value: ({
+    newPlot: (id, data, layout, params) => ({id, data, layout, params})
+  })
+})
