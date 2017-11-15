554546e9ae6a3394c2add488e02c20c6b47ca9a1
Hugo SCHOCH <schoch.hugo@gmail.com>
2017-11-15 14:10:11 +0100

    Add Edit Account / New|Edit|Delete Account Bil

--- src/actions/aws/accountsActions.js
+++ src/actions/aws/accountsActions.js
@@ -8,9 +8,28 @@ export default {
     type: Constants.AWS_NEW_ACCOUNT,
     account
   }),
+  newAccountBill: (accountID, bill) => ({
+    type: Constants.AWS_NEW_ACCOUNT_BILL,
+    accountID,
+    bill
+  }),
+  editAccount: (account) => ({
+    type: Constants.AWS_EDIT_ACCOUNT,
+    account
+  }),
+  editAccountBill: (accountID, bill) => ({
+    type: Constants.AWS_EDIT_ACCOUNT_BILL,
+    accountID,
+    bill
+  }),
   deleteAccount: (accountID) => ({
     type: Constants.AWS_DELETE_ACCOUNT,
     accountID
+  }),
+  deleteAccountBill: (accountID, bill) => ({
+    type: Constants.AWS_DELETE_ACCOUNT_BILL,
+    accountID,
+    bill
   }),
 	newExternal: () => ({
 		type: Constants.AWS_NEW_EXTERNAL
