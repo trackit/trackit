14e4836617f2a56ddf9035503732534188985c98
Hugo SCHOCH <schoch.hugo@gmail.com>
2017-11-15 14:11:50 +0100

    Add AWS API Calls

--- src/api/aws/accounts.js
+++ src/api/aws/accounts.js
@@ -8,8 +8,25 @@ export const newAccount = (account, token) => {
   return call('/aws', 'POST', account, token);
 };
 
+export const editAccount = (account, token) => {
+//  return call('/aws/' + accountID, 'DELETE', null, token);
+  return { status: true, data: {} };
+};
+
 export const deleteAccount = (accountID, token) => {
 //  return call('/aws/' + accountID, 'DELETE', null, token);
+  return { status: true, data: {} };
+};
+
+export const newAccountBill = (accountID, bill, token) => {
+  return { status: true, data: {} };
+};
+
+export const editAccountBill = (accountID, bill, token) => {
+  return { status: true, data: {} };
+};
+
+export const deleteAccountBill = (accountID, bill, token) => {
   return { status: true, data: {} };
 };
 
