94f609d6c6cd7941adbf36c4208c252cebc75c50
Hugo SCHOCH <schoch.hugo@gmail.com>
2017-12-11 11:23:43 +0100

    Update URL format

--- src/api/aws/accounts.js
+++ src/api/aws/accounts.js
@@ -5,7 +5,7 @@ export const getAccounts = (token) => {
 };
 
 export const getAccountBills = (accountID, token) => {
-//  return call('/aws/billrepository?aa=' + accountID, 'GET', null, token);
+//  return call(`/aws/billrepository?aa=${accountID}`, 'GET', null, token);
   return {
     success: true, data: [{
         "bucket": "s3://te.st",
@@ -22,24 +22,26 @@ export const newAccount = (account, token) => {
 };
 
 export const editAccount = (account, token) => {
-//  return call('/aws/' + accountID, 'DELETE', null, token);
+//  return call(`/aws?aa=${account.id}`, 'PATCH', null, token);
   return { success: true, data: {} };
 };
 
 export const deleteAccount = (accountID, token) => {
-//  return call('/aws/' + accountID, 'DELETE', null, token);
+//  return call(`/aws?aa=${accountID}`, 'DELETE', null, token);
   return { success: true, data: {} };
 };
 
 export const newAccountBill = (accountID, bill, token) => {
-  return call('/aws/billrepository?aa=' + accountID, 'POST', bill, token);
+  return call(`/aws/billrepository?aa=${accountID}`, 'POST', bill, token);
 };
 
 export const editAccountBill = (accountID, bill, token) => {
+//  return call(`/aws/billrepository?aa=${accountID}&br=${bill.id}`, 'PATCH', null, token);
   return { success: true, data: {} };
 };
 
 export const deleteAccountBill = (accountID, bill, token) => {
+//  return call(`/aws/billrepository?aa=${accountID}&br=${bill.id}`, 'DELETE', null, token);
   return { success: true, data: {} };
 };
 
