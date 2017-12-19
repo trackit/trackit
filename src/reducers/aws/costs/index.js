381637b780d83f2a1a5cd2a70b17207bfdd05fe0
Hugo SCHOCH <schoch.hugo@gmail.com>
2017-12-19 15:41:08 +0100

    Clean code

--- src/reducers/aws/costs/index.js
+++ src/reducers/aws/costs/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import values from './valuesReducer';
 import dates from './datesReducer';
 import interval from './intervalReducer';
+import filter from './filterReducer';
 
 export default combineReducers({
   values,
   dates,
-  interval
+  interval,
+  filter
 });
