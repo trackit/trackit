4c969ede2a4c3b33f7ae1553fcf50be9d186daf4
Hugo SCHOCH <schoch.hugo@gmail.com>
2017-12-19 15:37:56 +0100

    Add clear for AWS Costs actions

--- src/actions/aws/costsActions.js
+++ src/actions/aws/costsActions.js
@@ -15,8 +15,15 @@ export default {
       endDate
     }
   }),
+  clearCostsDates: () => ({type: Constants.AWS_CLEAR_COSTS_DATES}),
   setCostsInterval: (interval) => ({
     type: Constants.AWS_SET_COSTS_INTERVAL,
     interval
-  })
+  }),
+  clearCostsInterval: () => ({type: Constants.AWS_CLEAR_COSTS_INTERVAL}),
+  setCostsFilter: (filter) => ({
+    type: Constants.AWS_SET_COSTS_FILTER,
+    filter
+  }),
+  clearCostsFilter: () => ({type: Constants.AWS_CLEAR_COSTS_FILTER}),
 };
