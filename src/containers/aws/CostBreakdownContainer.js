e9084b5a1e1a6bb11368110e154203380e04a22c
Hugo SCHOCH <schoch.hugo@gmail.com>
2017-12-19 15:42:13 +0100

    Add Filter support in AWS.CostBreakdown Container

--- src/containers/aws/CostBreakdownContainer.js
+++ src/containers/aws/CostBreakdownContainer.js
@@ -1,39 +1,51 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import NVD3Chart from 'react-nvd3';
 import * as d3 from 'd3';
 import Components from "../../components";
 
 import Actions from "../../actions";
 
+import s3square from '../../assets/s3-square.png';
 import 'nvd3/build/nv.d3.min.css';
 
 const TimerangeSelector = Components.Misc.TimerangeSelector;
+const Selector = Components.Misc.Selector;
 const Panel = Components.Misc.Panel;
 
-class CostBreakdownContainer extends Component {
+export class CostBreakdownContainer extends Component {
 
   componentWillMount() {
-    this.props.getCosts(this.props.costsDates.startDate, this.props.costsDates.endDate, ["product", this.props.costsInterval]);
+    this.props.getCosts(this.props.costsDates.startDate, this.props.costsDates.endDate, [this.props.costsFilter, this.props.costsInterval]);
   }
 
   componentWillReceiveProps(nextProps) {
-    if (this.props.costsDates !== nextProps.costsDates || this.props.costsInterval !== nextProps.costsInterval)
-      nextProps.getCosts(nextProps.costsDates.startDate, nextProps.costsDates.endDate, ["product", nextProps.costsInterval]);
+    if (this.props.costsDates !== nextProps.costsDates ||
+      this.props.costsInterval !== nextProps.costsInterval ||
+      this.props.costsFilter !== nextProps.costsFilter)
+      nextProps.getCosts(nextProps.costsDates.startDate, nextProps.costsDates.endDate, [nextProps.costsFilter, nextProps.costsInterval]);
   }
 
-  transformProducts = (data, timescope) => {
+  transformProducts = (data, filter, interval) => {
+    if (filter === "all" && data.hasOwnProperty(interval))
+      return [{
+        key: "Total",
+        values: Object.keys(data[interval]).map((date) => ([date, data[interval][date]]))
+      }];
+    else if (!data.hasOwnProperty(filter))
+      return [];
+    let dates = [];
     try {
-      let dates = [];
-      Object.keys(data.product).forEach((key) => {
-        Object.keys(data.product[key][timescope]).forEach((date) => {
+      Object.keys(data[filter]).forEach((key) => {
+        Object.keys(data[filter][key][interval]).forEach((date) => {
           if (dates.indexOf(date) === -1)
             dates.push(date);
         })
       });
-      return Object.keys(data.product).map((key) => ({
-        key,
-        values: dates.map((date) => ([date, data.product[key][timescope][date] || 0]))
+      return Object.keys(data[filter]).map((key) => ({
+        key: (key.length ? key : `No ${filter}`),
+        values: dates.map((date) => ([date, data[filter][key][interval][date] || 0]))
       }));
     } catch (e) {
       return [];
@@ -42,17 +54,25 @@ class CostBreakdownContainer extends Component {
 
   render() {
 
+    const filters = {
+      all: "Total",
+      account: "Account",
+      product: "Product",
+      region: "Region"
+    };
+
+    /* istanbul ignore next */
     const context = {
       formatXAxis: (d) => (d3.time.format('%x')(new Date(d))),
       formatYAxis: (d) => ('$' + d3.format(',.2f')(d)),
     };
 
-    const datum = (this.props.costsValues && this.props.costsInterval ? this.transformProducts(this.props.costsValues, this.props.costsInterval) : null);
+    const datum = (this.props.costsValues && this.props.costsInterval && this.props.costsFilter ? this.transformProducts(this.props.costsValues, this.props.costsFilter, this.props.costsInterval) : null);
 
-    const chart = (this.props.costsValues && this.props.costsInterval ? (
+    const chart = (datum ? (
       <NVD3Chart
         id="barChart"
-        type={this.props.horizontal ? 'multiBarHorizontalChart' : 'multiBarChart'}
+        type="multiBarChart"
         datum={datum}
         context={context}
         xAxis={{
@@ -67,17 +87,21 @@ class CostBreakdownContainer extends Component {
             type:'function',
           }
         }}
-        margin={this.props.horizontal ? {left: 100} : {right:100}}
+        margin={{right:100}}
         rightAlignYAxis={true}
         clipEdge={true}
         showControls={true}
-        stacked={this.props.horizontal}
-        x={(d) => {
+        x={
+          /* istanbul ignore next */
+          (d) => {
           const date = new Date(d[0]);
           return date.getTime();
         }}
-        y={(d) => (d[1])}
-        height={this.props.horizontal ? 850 : 400}
+        y={
+          /* istanbul ignore next */
+          (d) => (d[1])
+        }
+        height={400}
       />
     ) : null);
 
@@ -86,6 +110,7 @@ class CostBreakdownContainer extends Component {
 
         <div>
           <h3 className="white-box-title no-padding inline-block">
+            <img className="white-box-title-icon" src={s3square} alt="AWS square logo"/>
             Cost Breakdown
           </h3>
           <div className="inline-block pull-right">
@@ -95,6 +120,13 @@ class CostBreakdownContainer extends Component {
               setDatesFunc={this.props.setCostsDates}
               interval={this.props.costsInterval}
               setIntervalFunc={this.props.setCostsInterval}
+            />
+          </div>
+          <div className="inline-block pull-right">
+            <Selector
+              values={filters}
+              selected={this.props.costsFilter}
+              selectValue={this.props.setCostsFilter}
             />
           </div>
         </div>
@@ -108,11 +140,26 @@ class CostBreakdownContainer extends Component {
   }
 }
 
+CostBreakdownContainer.propTypes = {
+  costsValues: PropTypes.object,
+  costsDates: PropTypes.shape({
+    startDate: PropTypes.object,
+    endDate: PropTypes.object,
+  }),
+  costsInterval:PropTypes.string.isRequired,
+  costsFilter:PropTypes.string.isRequired,
+  getCosts: PropTypes.func.isRequired,
+  setCostsDates: PropTypes.func.isRequired,
+  setCostsInterval: PropTypes.func.isRequired,
+  setCostsFilter: PropTypes.func.isRequired,
+};
+
 /* istanbul ignore next */
 const mapStateToProps = ({aws}) => ({
   costsValues: aws.costs.values,
   costsDates: aws.costs.dates,
-  costsInterval: aws.costs.interval
+  costsInterval: aws.costs.interval,
+  costsFilter: aws.costs.filter
 });
 
 /* istanbul ignore next */
@@ -125,6 +172,9 @@ const mapDispatchToProps = (dispatch) => ({
   },
   setCostsInterval: (interval) => {
     dispatch(Actions.AWS.Costs.setCostsInterval(interval));
+  },
+  setCostsFilter: (filter) => {
+    dispatch(Actions.AWS.Costs.setCostsFilter(filter));
   }
 });
 
