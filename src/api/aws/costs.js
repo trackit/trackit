947381370d406b1cdc2aed11fab81c4c6fb70f43
Hugo SCHOCH <schoch.hugo@gmail.com>
2017-12-19 15:38:16 +0100

    Add support for "Total" filter in AWS.Costs

--- src/api/aws/costs.js
+++ src/api/aws/costs.js
@@ -1,6 +1,7 @@
 import { call } from './../misc.js';
 
 export const getCosts = (token, begin, end, filters, accounts=undefined) => {
+  filters = filters.filter((item) => (item !== "all"));
   let route = `/costs?begin=${begin.format("YYYY-MM-DD")}&end=${end.format("YYYY-MM-DD")}&by=${filters.join(',')}`;
   if (accounts)
     route += `&accounts=${accounts.join(',')}`;
