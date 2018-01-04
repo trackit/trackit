6256abd82da1601c5a3414af7be6d3c72432dc43
Hugo SCHOCH <schoch.hugo@gmail.com>
2018-01-04 16:41:27 +0100

    Add regex to get ID from RoleARN

--- src/common/forms/AWSAccountForm.js
+++ src/common/forms/AWSAccountForm.js
@@ -2,9 +2,14 @@ import React from 'react';
 import Misc from "./MiscForm";
 
 const roleArnFormat = (value) => {
-const regex = /arn:aws:iam::[\d]{12}:role\/(?:[a-zA-Z0-9+=,.@_-](?:\/[a-zA-Z0-9+=,.@_-])?)+/;
-if (!regex.exec(value))
-  return (<div className="alert alert-warning">{value} is not a valid role ARN.</div>);
+  const regex = /arn:aws:iam::[\d]{12}:role\/(?:[a-zA-Z0-9+=,.@_-](?:\/[a-zA-Z0-9+=,.@_-])?)+/;
+  if (!regex.exec(value))
+    return (<div className="alert alert-warning">{value} is not a valid role ARN.</div>);
+};
+
+const getAccountIDFromRole = (value) => {
+  const regex = /arn:aws:iam::(.*?):role\/(?:[a-zA-Z0-9+=,.@_-](?:\/[a-zA-Z0-9+=,.@_-])?)+/;
+  return regex.exec(value)[1];
 };
 
 const s3BucketFormat = (value) => {
@@ -23,6 +28,7 @@ const pathFormat = (value) => {
 export default {
   required: Misc.required,
   roleArnFormat,
+  getAccountIDFromRole,
   s3BucketFormat,
   pathFormat
 };
