5b84010868fd29a695d9f5e12be01f6208c608d8
Hugo SCHOCH <schoch.hugo@gmail.com>
2018-01-09 15:28:46 +0100

    Add tutorial in Wizard

---
+++ src/components/misc/Picture.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import Dialog, {DialogContent} from 'material-ui/Dialog';
+
+class Picture extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+    this.openDialog = this.openDialog.bind(this);
+    this.closeDialog = this.closeDialog.bind(this);
+  }
+
+  openDialog = (e) => {
+    e.preventDefault();
+    this.setState({open: true});
+  };
+
+  closeDialog = (e) => {
+    e.preventDefault();
+    this.setState({open: false});
+  };
+
+  render() {
+
+    let image = <img className="white-box-title-icon" src={this.props.src} alt={this.props.alt}/>;
+
+    let button = (this.props.preview ? image : (
+      <button className="btn btn-default">{this.props.text}</button>
+    ));
+
+    return(
+      <div className="picture">
+
+        <div onClick={this.openDialog}>
+          {button}
+        </div>
+
+        <Dialog open={this.state.open} fullWidth maxWidth={false}>
+
+          <DialogContent className="picture" onClick={this.closeDialog}>
+            {image}
+          </DialogContent>
+
+        </Dialog>
+
+      </div>
+    );
+  }
+
+}
+
+Picture.propTypes = {
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string,
+  text: PropTypes.string,
+  preview: PropTypes.bool
+};
+
+Picture.defaultProps = {
+  text: "See picture",
+  alt: "",
+  preview: false
+};
+
+export default Picture;
