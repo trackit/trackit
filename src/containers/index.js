e8285a6c7702fca538086bf8809eb2b255c7f412
Hugo SCHOCH <schoch.hugo@gmail.com>
2018-01-16 16:40:21 +0100

    Add Home Container

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,10 +1,12 @@
 import Main from './MainContainer';
+import Home from './HomeContainer';
 import Auth from './auth';
 import Setup from './setup';
 import AWS from './aws';
 
 export default {
   Main,
+  Home,
   Auth,
   Setup,
   AWS,
