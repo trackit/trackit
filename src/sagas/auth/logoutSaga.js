cef75300c4fa70ffea5acb9e8fecd0642f3a38dc
Hugo SCHOCH <schoch.hugo@gmail.com>
2018-02-12 10:32:30 +0100

    Clean sagas

--- src/sagas/auth/logoutSaga.js
+++ src/sagas/auth/logoutSaga.js
@@ -1,13 +1,8 @@
 import { put, all } from 'redux-saga/effects';
-import { getToken, getUserMail } from '../../common/localStorage';
 import Constants from '../../constants';
 
 export default function* logoutSaga() {
   try {
-    if (!getToken())
-      throw Error("No token available");
-    if (!getUserMail())
-      throw Error("No user mail available");
     yield all([
       put({ type: Constants.LOGOUT_REQUEST_SUCCESS }),
       put({ type: Constants.CLEAN_USER_TOKEN }),
