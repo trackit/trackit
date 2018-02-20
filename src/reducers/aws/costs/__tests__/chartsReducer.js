cb6f8c6b9204629472e7c1d8854125eb61ba7749
Hugo SCHOCH <schoch.hugo@gmail.com>
2018-02-20 15:03:32 +0100

    Update tests

--- src/reducers/aws/costs/__tests__/chartsReducer.js
+++ src/reducers/aws/costs/__tests__/chartsReducer.js
@@ -4,23 +4,24 @@ import Constants from '../../../../constants';
 describe("ChartsReducer", () => {
 
   const id = "id";
-  const state = [id];
+  const chartType = "type";
+  const state = {id: chartType};
   const insert = state;
 
   it("handles initial state", () => {
-    expect(ChartsReducer(undefined, {})).toEqual([]);
+    expect(ChartsReducer(undefined, {})).toEqual({});
   });
 
   it("handles insert dates state", () => {
-    expect(ChartsReducer([], { type: Constants.AWS_INSERT_CHARTS, charts: insert })).toEqual(insert);
+    expect(ChartsReducer({}, { type: Constants.AWS_INSERT_CHARTS, charts: insert })).toEqual(insert);
   });
 
   it("handles add chart state", () => {
-    expect(ChartsReducer([], { type: Constants.AWS_ADD_CHART, id })).toEqual(state);
+    expect(ChartsReducer({}, { type: Constants.AWS_ADD_CHART, id, chartType})).toEqual(state);
   });
 
   it("handles remove chart state", () => {
-    expect(ChartsReducer(state, { type: Constants.AWS_REMOVE_CHART, id })).toEqual([]);
+    expect(ChartsReducer(state, { type: Constants.AWS_REMOVE_CHART, id })).toEqual({});
     expect(ChartsReducer(state, { type: Constants.AWS_REMOVE_CHART, id: 42 })).toEqual(state);
   });
 
