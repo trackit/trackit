10218078a7890d29f44964fed0fe167e64c5c951
Hugo SCHOCH <schoch.hugo@gmail.com>
2018-03-13 14:23:43 +0100

    Clean code

--- src/components/aws/s3Analytics/TableComponent.js
+++ src/components/aws/s3Analytics/TableComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import ReactTable from "react-table";
+import ReactTable from 'react-table';
 import Spinner from 'react-spinkit';
 
 import {formatGigaBytes, formatPrice} from '../../../common/formatters';
