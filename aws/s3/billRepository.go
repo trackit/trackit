a14cdda2a3ec406c73e66d6f2b6220fa2af6b086
Simon Meyer <simon.meyer@epitech.eu>
2018-07-11 18:18:58 +0200

    add status for bill repository on db and api

--- aws/s3/billRepository.go
+++ aws/s3/billRepository.go
@@ -102,6 +102,7 @@ type BillRepository struct {
 	AwsAccountId         int       `json:"awsAccountId"`
 	Bucket               string    `json:"bucket"`
 	Prefix               string    `json:"prefix"`
+	Status               string    `json:"status"`
 	LastImportedManifest time.Time `json:"lastImportedManifest"`
 	NextUpdate           time.Time `json:"nextUpdate"`
 }
@@ -193,6 +194,7 @@ func billRepoFromDbBillRepo(dbBillRepo models.AwsBillRepository) BillRepository 
 		Id:                   dbBillRepo.ID,
 		Bucket:               dbBillRepo.Bucket,
 		Prefix:               dbBillRepo.Prefix,
+		Status:               dbBillRepo.Status,
 		AwsAccountId:         dbBillRepo.AwsAccountID,
 		LastImportedManifest: dbBillRepo.LastImportedManifest,
 		NextUpdate:           dbBillRepo.NextUpdate,
@@ -204,6 +206,7 @@ func dbBillRepoFromBillRepo(br BillRepository) models.AwsBillRepository {
 		ID:                   br.Id,
 		Bucket:               br.Bucket,
 		Prefix:               br.Prefix,
+		Status:               br.Status,
 		AwsAccountID:         br.AwsAccountId,
 		LastImportedManifest: br.LastImportedManifest,
 		NextUpdate:           br.NextUpdate,
