c00d33ccb76defdb9ba55d5fad4292cc77388bcc
Hugo SCHOCH <schoch.hugo@gmail.com>
2018-08-17 14:21:00 +0200

    Update accounts parameters in API calls

--- src/api/aws/reports.js
+++ src/api/aws/reports.js
@@ -1,11 +1,11 @@
 import { call, download } from "../misc";
 
 export const getReports = (token, accountId) => {
-  let route = `/reports?aa=${accountId}`;
+  let route = `/reports?account-id=${accountId}`;
   return call(route, 'GET', null, token);
 };
 
 export const getReport = (token, accountId, reportType, fileName) => {
-  let route = `/report?aa=${accountId}&report-type=${reportType}&file-name=${fileName}`;
+  let route = `/report?account-id=${accountId}&report-type=${reportType}&file-name=${fileName}`;
   return download(route, 'GET', null, token, 'application/vnd.ms-excel');
-}
+};
--- src/api/aws/s3.js
+++ src/api/aws/s3.js
@@ -3,6 +3,6 @@ import { call } from "../misc";
 export const getData = (token, begin, end, accounts=undefined) => {
   let route = `/s3/costs?begin=${begin.format("YYYY-MM-DD")}&end=${end.format("YYYY-MM-DD")}`;
   if (accounts && accounts.length)
-    route += `&aas=${accounts.join(',')}`;
+    route += `&accounts=${accounts.join(',')}`;
   return call(route, 'GET', null, token);
 };
