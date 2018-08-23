16db45eaca8a595e9470c1f2431f3209a43fa17e
romain zanchi <romainzanchi30@gmail.com>
2018-08-23 17:08:34 +0200

    added ES account sharing on the ec2 route

--- ec2/ec2_route.go
+++ ec2/ec2_route.go
@@ -16,13 +16,14 @@ package ec2
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/trackit/jsonlog"
 	"gopkg.in/olivere/elastic.v5"
 
-	"github.com/trackit/trackit-server/aws"
 	"github.com/trackit/trackit-server/aws/ec2"
 	"github.com/trackit/trackit-server/db"
 	"github.com/trackit/trackit-server/es"
@@ -33,6 +34,7 @@ import (
 // esQueryParams will store the parsed query params
 type esQueryParams struct {
 	accountList []string
+	indexList   []string
 }
 
 // ec2QueryArgs allows to get required queryArgs params
@@ -61,9 +63,9 @@ func init() {
 // the user (e.g if the index does not exists because it was not yet indexed ) the error will
 // be returned, but instead of having a 500 status code, it will return the provided status code
 // with empy data
-func makeElasticSearchRequest(ctx context.Context, parsedParams esQueryParams, user users.User) (*elastic.SearchResult, int, error) {
+func makeElasticSearchRequest(ctx context.Context, parsedParams esQueryParams) (*elastic.SearchResult, int, error) {
 	l := jsonlog.LoggerFromContextOrDefault(ctx)
-	index := es.IndexNameForUser(user, ec2.IndexPrefixEC2Report)
+	index := strings.Join(parsedParams.indexList, ",")
 	searchService := GetElasticSearchParams(
 		parsedParams.accountList,
 		es.Client,
@@ -90,10 +92,14 @@ func getEc2Instances(request *http.Request, a routes.Arguments) (int, interface{
 	if a[ec2QueryArgs[0]] != nil {
 		parsedParams.accountList = a[ec2QueryArgs[0]].([]string)
 	}
-	if err := aws.ValidateAwsAccounts(parsedParams.accountList); err != nil {
-		return http.StatusBadRequest, err
+	tx := a[db.Transaction].(*sql.Tx)
+	accountsAndIndexes, returnCode, err := es.GetAccountsAndIndexes(parsedParams.accountList, user, tx, ec2.IndexPrefixEC2Report)
+	if err != nil {
+		return returnCode, err
 	}
-	searchResult, returnCode, err := makeElasticSearchRequest(request.Context(), parsedParams, user)
+	parsedParams.accountList = accountsAndIndexes.Accounts
+	parsedParams.indexList = accountsAndIndexes.Indexes
+	searchResult, returnCode, err := makeElasticSearchRequest(request.Context(), parsedParams)
 	if err != nil {
 		return returnCode, err
 	}
