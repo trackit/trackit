5734d5b75ca4c8505fca816b9422d22ba7bcec4d
Colin Cleary <radiodouf@gmail.com>
2018-09-04 11:43:31 +0200

    Fixing issue with registration with aws token

--- db/schema.sql
+++ db/schema.sql
@@ -333,4 +333,4 @@ CREATE TABLE shared_account (
 --   See the License for the specific language governing permissions and
 --   limitations under the License.
 
-ALTER TABLE user ADD aws_customer_entitlement bool NOT NULL DEFAULT 0;
+ALTER TABLE user ADD aws_customer_entitlement bool NOT NULL DEFAULT 1;
