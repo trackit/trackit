2db39342aed7b24f61f249f7159b1c1439c77717
romain zanchi <romainzanchi30@gmail.com>
2018-10-02 17:28:32 +0200

    added plugins endpoints

---
+++ plugins/account/core/prepare_response.go
@@ -0,0 +1,65 @@
+//   Copyright 2017 MSolution.IO
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+package plugins_account_core
+
+import (
+	"context"
+	"encoding/json"
+
+	"gopkg.in/olivere/elastic.v5"
+
+	"github.com/trackit/jsonlog"
+)
+
+type bucket = map[string]interface{}
+
+// parseBuckets iterates through all the buckets to retrieve the top hits
+func parseBuckets(reports []interface{}, parsedTopReports bucket) []interface{} {
+	buckets := parsedTopReports["buckets"].([]interface{})
+	for _, bucketTmp := range buckets {
+		bucketData := bucketTmp.(bucket)
+		bucketTopHits := bucketData["top_reports_hits"].(bucket)
+		topHitsHits := bucketTopHits["hits"].(bucket)
+		hitsList := topHitsHits["hits"].([]interface{})
+		if len(hitsList) > 0 {
+			topHit := hitsList[0].(bucket)
+			reports = append(reports, topHit["_source"])
+		}
+	}
+	return reports
+}
+
+// parseESResult parses an *elastic.SearchResult according to it's resultType
+func parseESResult(ctx context.Context, res *elastic.SearchResult) ([]interface{}, error) {
+	logger := jsonlog.LoggerFromContextOrDefault(ctx)
+	reports := make([]interface{}, 0)
+	var parsedTopReports bucket
+	err := json.Unmarshal(*res.Aggregations["top_plugins_account"], &parsedTopReports)
+	if err != nil {
+		logger.Error("Failed to parse elasticsearch document.", err.Error())
+		return reports, err
+	}
+	reports = parseBuckets(reports, parsedTopReports)
+	return reports, nil
+}
+
+// prepareResponse parses the results from elasticsearch and returns the plugins results
+func prepareResponse(ctx context.Context, res *elastic.SearchResult) (interface{}, error) {
+	reports, err := parseESResult(ctx, res)
+	if err != nil {
+		return nil, err
+	}
+	return reports, nil
+}
---
+++ plugins/account/core/route.go
@@ -0,0 +1,110 @@
+//   Copyright 2018 MSolution.IO
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+package plugins_account_core
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/trackit/jsonlog"
+	"gopkg.in/olivere/elastic.v5"
+
+	"github.com/trackit/trackit-server/db"
+	"github.com/trackit/trackit-server/es"
+	"github.com/trackit/trackit-server/routes"
+	"github.com/trackit/trackit-server/users"
+)
+
+// pluginsQueryParams will store the parsed query params
+type pluginsQueryParams struct {
+	accountList []string
+	indexList   []string
+}
+
+// pluginsQueryArgs allows to get required queryArgs params
+var pluginsQueryArgs = []routes.QueryArg{
+	routes.AwsAccountsOptionalQueryArg,
+}
+
+func init() {
+	routes.MethodMuxer{
+		http.MethodGet: routes.H(getPluginsResults).With(
+			db.RequestTransaction{Db: db.Db},
+			users.RequireAuthenticatedUser{users.ViewerAsParent},
+			routes.QueryArgs(pluginsQueryArgs),
+			routes.Documentation{
+				Summary:     "get the latests plugins results",
+				Description: "Responds with the latests plugins results for the account(s) specified in the request",
+			},
+		),
+	}.H().Register("/plugins/results")
+}
+
+// makeElasticSearchPluginsRequest prepares and run the request to retrieve the latest plugins results
+// based on the pluginsQueryParams
+// It will return the data, an http status code (as int) and an error.
+// Because an error can be generated, but is not critical and is not needed to be known by
+// the user (e.g if the index does not exists because it was not yet indexed ) the error will
+// be returned, but instead of having a 500 status code, it will return the provided status code
+// with empy data
+func makeElasticSearchPluginsRequest(ctx context.Context, parsedParams pluginsQueryParams) (*elastic.SearchResult, int, error) {
+	l := jsonlog.LoggerFromContextOrDefault(ctx)
+	index := strings.Join(parsedParams.indexList, ",")
+	searchService := GetElasticSearchPluginsParams(
+		parsedParams.accountList,
+		es.Client,
+		index,
+	)
+	res, err := searchService.Do(ctx)
+	if err != nil {
+		if elastic.IsNotFound(err) {
+			l.Warning("Query execution failed, ES index does not exists : "+index, err)
+			return nil, http.StatusOK, err
+		}
+		l.Error("Query execution failed : "+err.Error(), nil)
+		return nil, http.StatusInternalServerError, fmt.Errorf("could not execute the ElasticSearch query")
+	}
+	return res, http.StatusOK, nil
+}
+
+// getPluginsResults returns the list of plugins results based on the query params, in JSON format.
+func getPluginsResults(request *http.Request, a routes.Arguments) (int, interface{}) {
+	user := a[users.AuthenticatedUser].(users.User)
+	parsedParams := pluginsQueryParams{
+		accountList: []string{},
+	}
+	if a[pluginsQueryArgs[0]] != nil {
+		parsedParams.accountList = a[pluginsQueryArgs[0]].([]string)
+	}
+	tx := a[db.Transaction].(*sql.Tx)
+	accountsAndIndexes, returnCode, err := es.GetAccountsAndIndexes(parsedParams.accountList, user, tx, IndexPrefixAccountPlugin)
+	if err != nil {
+		return returnCode, err
+	}
+	parsedParams.accountList = accountsAndIndexes.Accounts
+	parsedParams.indexList = accountsAndIndexes.Indexes
+	pluginsResult, returnCode, err := makeElasticSearchPluginsRequest(request.Context(), parsedParams)
+	if err != nil {
+		return returnCode, err
+	}
+	res, err := prepareResponse(request.Context(), pluginsResult)
+	if err != nil {
+		return http.StatusInternalServerError, err
+	}
+	return http.StatusOK, res
+}
