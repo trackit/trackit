3c69e6ebda68e446bbeee6d0b2bbd9662bea4b49
Hugo SCHOCH <schoch.hugo@gmail.com>
2018-12-03 12:58:21 +0100

    Fix Tags support for ElasticSearch in AWS Resources

--- src/components/aws/resources/ElasticSearchComponent.js
+++ src/components/aws/resources/ElasticSearchComponent.js
@@ -9,6 +9,7 @@ import Popover from '@material-ui/core/Popover';
 import {formatPercent, formatPrice, formatMegaBytes, formatGigaBytes} from '../../../common/formatters';
 import Misc from '../../misc';
 import Costs from "./misc/Costs";
+import Tags from './misc/Tags';
 
 const Tooltip = Misc.Popover;
 
@@ -16,68 +17,6 @@ const getTotalCost = (costs) => {
   let total = 0;
   Object.keys(costs).forEach((key) => total += costs[key]);
   return total;
-};
-
-export class Tags extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      showPopOver: false
-    };
-    this.handlePopoverOpen = this.handlePopoverOpen.bind(this);
-    this.handlePopoverClose = this.handlePopoverClose.bind(this);
-  }
-
-  handlePopoverOpen = (e) => {
-    e.preventDefault();
-    this.setState({ showPopOver: true });
-  };
-
-  handlePopoverClose = (e) => {
-    e.preventDefault();
-    this.setState({ showPopOver: false });
-  };
-
-  render() {
-    return (
-      <div>
-        <Popover
-          open={this.state.showPopOver}
-          anchorEl={this.anchor}
-          onClose={this.handlePopoverClose}
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'center',
-          }}
-          transformOrigin={{
-            vertical: 'top',
-            horizontal: 'right',
-          }}
-        >
-          <div
-            className="tags-list"
-            onClick={this.handlePopoverClose}
-          >
-            {Object.keys(this.props.tags).map((tag, index) => (<div key={index} className="tags-item">{tag} : {this.props.tags[tag]}</div>))}
-          </div>
-        </Popover>
-        <div
-          ref={node => {
-            this.anchor = node;
-          }}
-          onClick={this.handlePopoverOpen}
-        >
-          <Tooltip placement="left" icon={<i className="fa fa-tags"/>} tooltip="Click to show tags"/>
-        </div>
-      </div>
-    );
-  }
-
-}
-
-Tags.propTypes = {
-  tags: PropTypes.object.isRequired
 };
 
 export class ElasticSearchComponent extends Component {
