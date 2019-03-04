35152e16816a4b35f125fd39a38dc3c42befd1b3
Victor GIUBILEI <Victor.giubilei@gmail.com>
2019-03-04 16:31:38 +0100

    Created the route for getting Untagged resources

--- costs/tags/tags.go
+++ costs/tags/tags.go
@@ -28,6 +28,71 @@ import (
 	"github.com/trackit/trackit/users"
 )
 
+// TagsValuesQueryParams will store the parsed query params for /tags/values endpoint
+type TagsValuesQueryParams struct {
+	AccountList []string  `json:"awsAccounts"`
+	IndexList   []string  `json:"indexes"`
+	DateBegin   time.Time `json:"begin"`
+	DateEnd     time.Time `json:"end"`
+	TagsKeys    []string  `json:"keys"`
+	By          string    `json:"by"`
+	Detailed    bool      `json:"detailed"`
+}
+
+// tagsKeysQueryParams will store the parsed query params for /tags/keys endpoint
+type TagsKeysQueryParams struct {
+	AccountList []string  `json:"awsAccounts"`
+	IndexList   []string  `json:"indexes"`
+	DateBegin   time.Time `json:"begin"`
+	DateEnd     time.Time `json:"end"`
+}
+
+type untaggedQueryParams struct {
+	AccountList []string  `json:"awsAccounts"`
+	IndexList   []string  `json:"indexes"`
+	DateBegin   time.Time `json:"begin "`
+	DateEnd     time.Time `json:"end"`
+	TagKey      string    `json:"key"`
+}
+
+// tagsValuesQueryArgs allows to get required queryArgs params for /tags/values endpoint
+var tagsValuesQueryArgs = []routes.QueryArg{
+	routes.AwsAccountsOptionalQueryArg,
+	routes.DateBeginQueryArg,
+	routes.DateEndQueryArg,
+	routes.QueryArg{
+		Name:        "keys",
+		Description: "keys of the tags to search",
+		Type:        routes.QueryArgStringSlice{},
+		Optional:    true,
+	},
+	routes.QueryArg{
+		Name:        "by",
+		Description: "Criteria for the ES aggregation: product, availabilityzone, region or account.",
+		Type:        routes.QueryArgString{},
+		Optional:    false,
+	},
+}
+
+// tagsKeysQueryArgs allows to get required queryArgs params for /tags/keys endpoint
+var tagsKeysQueryArgs = []routes.QueryArg{
+	routes.AwsAccountsOptionalQueryArg,
+	routes.DateBeginQueryArg,
+	routes.DateEndQueryArg,
+}
+
+var untaggedQueryArgs = []routes.QueryArg{
+	routes.AwsAccountsOptionalQueryArg,
+	routes.DateBeginQueryArg,
+	routes.DateEndQueryArg,
+	routes.QueryArg{
+		Name:        "key",
+		Description: "Tag key to check the untagged resources against",
+		Type:        routes.QueryArgString{},
+		Optional:    false,
+	},
+}
+
 func init() {
 	routes.MethodMuxer{
 		http.MethodGet: routes.H(getTagsValues).With(
@@ -53,43 +118,17 @@ func init() {
 			},
 		),
 	}.H().Register("/costs/tags/keys")
-}
-
-// tagsValuesQueryArgs allows to get required queryArgs params for /tags/values endpoint
-var tagsValuesQueryArgs = []routes.QueryArg{
-	routes.AwsAccountsOptionalQueryArg,
-	routes.DateBeginQueryArg,
-	routes.DateEndQueryArg,
-	routes.QueryArg{
-		Name:        "keys",
-		Description: "keys of the tags to search",
-		Type:        routes.QueryArgStringSlice{},
-		Optional:    true,
-	},
-	routes.QueryArg{
-		Name:        "by",
-		Description: "Criteria for the ES aggregation: product, availabilityzone, region or account.",
-		Type:        routes.QueryArgString{},
-		Optional:    false,
-	},
-	routes.DetailedQueryArg,
-	routes.QueryArg{
-		Name:        "Detailed",
-		Description: "Specify if the report will be detailed or not",
-		Type:        routes.QueryArgBool{},
-		Optional:    true,
-	},
-}
-
-// TagsValuesQueryParams will store the parsed query params for /tags/values endpoint
-type TagsValuesQueryParams struct {
-	AccountList []string  `json:"awsAccounts"`
-	IndexList   []string  `json:"indexes"`
-	DateBegin   time.Time `json:"begin"`
-	DateEnd     time.Time `json:"end"`
-	TagsKeys    []string  `json:"keys"`
-	By          string    `json:"by"`
-	Detailed    bool      `json:"detailed"`
+	routes.MethodMuxer{
+		http.MethodGet: routes.H(getUntaggedResources).With(
+			db.RequestTransaction{Db: db.Db},
+			users.RequireAuthenticatedUser{users.ViewerAsParent},
+			routes.QueryArgs(untaggedQueryArgs),
+			routes.Documentation{
+				Summary:     "get list of untagged resources",
+				Description: "get list of either tagged resources with empty value, or untagged resources, with filter for a specified time range, aws account and key",
+			},
+		),
+	}.H().Register("/costs/tags/untagged")
 }
 
 // getTagsValues returns tags and their values (cost) based on the query params, in JSON format.
@@ -130,21 +169,6 @@ func getTagsValues(request *http.Request, a routes.Arguments) (int, interface{})
 	return returnCode, err
 }
 
-// tagsKeysQueryArgs allows to get required queryArgs params for /tags/keys endpoint
-var tagsKeysQueryArgs = []routes.QueryArg{
-	routes.AwsAccountsOptionalQueryArg,
-	routes.DateBeginQueryArg,
-	routes.DateEndQueryArg,
-}
-
-// TagsKeysQueryParams will store the parsed query params for /tags/keys endpoint
-type TagsKeysQueryParams struct {
-	AccountList []string  `json:"awsAccounts"`
-	IndexList   []string  `json:"indexes"`
-	DateBegin   time.Time `json:"begin"`
-	DateEnd     time.Time `json:"end"`
-}
-
 // getTagsKeys returns the list of the tag keys based on the query params, in JSON format.
 func getTagsKeys(request *http.Request, a routes.Arguments) (int, interface{}) {
 	user := a[users.AuthenticatedUser].(users.User)
@@ -169,4 +193,27 @@ func getTagsKeys(request *http.Request, a routes.Arguments) (int, interface{}) {
 		return returnCode, res
 	}
 	return returnCode, err
+}
+
+// getUntaggedResources will return the list of resources that have an empty tag
+func getUntaggedResources(request *http.Request, a routes.Arguments) (int, interface{}) {
+	user := a[users.AuthenticatedUser].(users.User)
+	parsedParams := untaggedQueryParams{
+		AccountList: []string{},
+		IndexList:   []string{},
+		DateBegin:   a[untaggedQueryArgs[1]].(time.Time),
+		DateEnd:     a[untaggedQueryArgs[2]].(time.Time).Add(time.Hour*time.Duration(23) + time.Minute*time.Duration(59) + time.Second*time.Duration(59)),
+		TagKey:      a[untaggedQueryArgs[3]].(string),
+	}
+	if a[untaggedQueryArgs[0]] != nil {
+		parsedParams.AccountList = a[untaggedQueryArgs[0]].([]string)
+	}
+	tx := a[db.Transaction].(*sql.Tx)
+	accountsAndIndexes, returnCode, err := es.GetAccountsAndIndexes(parsedParams.AccountList, user, tx, s3.IndexPrefixLineItem)
+	if err != nil {
+		return returnCode, err
+	}
+	parsedParams.AccountList = accountsAndIndexes.Accounts
+	parsedParams.IndexList = accountsAndIndexes.Indexes
+	return getUntaggedResourcesWithParsedParams(request.Context(), parsedParams)
 }
