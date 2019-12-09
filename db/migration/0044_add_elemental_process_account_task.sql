46081c367841209074bcfaf980c6513a1958a037
Jean-Gaël Choppe <jean-gael.choppe@epitech.eu>
2019-12-09 12:16:37 +0100

    add db migration file

---
+++ db/migration/0044_add_elemental_process_account_task.sql
@@ -0,0 +1,32 @@
+--   Copyright 2017 MSolution.IO
+--
+--   Licensed under the Apache License, Version 2.0 (the "License");
+--   you may not use this file except in compliance with the License.
+--   You may obtain a copy of the License at
+--
+--       http://www.apache.org/licenses/LICENSE-2.0
+--
+--   Unless required by applicable law or agreed to in writing, software
+--   distributed under the License is distributed on an "AS IS" BASIS,
+--   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--   See the License for the specific language governing permissions and
+--   limitations under the License.
+
+CREATE TABLE aws_account_elemental_update_job (
+	id                     INTEGER      NOT NULL AUTO_INCREMENT,
+	created                TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	aws_account_id 				 INTEGER      NOT NULL,
+	completed              TIMESTAMP    NOT NULL DEFAULT 0,
+	worker_id              VARCHAR(255) NOT NULL,
+	jobError               VARCHAR(255) NOT NULL DEFAULT "",
+	historyError               VARCHAR(255) NOT NULL DEFAULT "",
+	CONSTRAINT PRIMARY KEY (id),
+	CONSTRAINT foreign_aws_account FOREIGN KEY (aws_account_id) REFERENCES aws_account(id) ON DELETE CASCADE
+);
+
+ALTER TABLE aws_account ADD next_elemental_update DATETIME NOT NULL DEFAULT "1970-01-01 00:00:00";
+ALTER TABLE aws_account ADD last_elemental_update DATETIME NOT NULL DEFAULT "1970-01-01 00:00:00";
+
+CREATE VIEW aws_account_due_elemental_update AS
+	SELECT * FROM aws_account WHERE next_elemental_update <= NOW() AND last_elemental_update <= NOW()
+;
--- server/taskElementalProcessAccount.go
+++ server/taskElementalProcessAccount.go
@@ -26,13 +26,12 @@ import (
 
 	"github.com/trackit/trackit/aws"
 	"github.com/trackit/trackit/aws/usageReports/history"
-	"github.com/trackit/trackit/cache"
 	"github.com/trackit/trackit/db"
 )
 
 func checkArgumentsForElementalProcessAccount(args []string) (int, time.Time, error) {
 	if len(args) < 1 {
-		return invalidAccId, invalidTime, errors.New("process account task requires at least an integer argument as AWS Account UD")
+		return invalidAccId, invalidTime, errors.New("elemental process account task requires at least an integer argument as AWS Account UD")
 	}
 	accId, err := strconv.Atoi(args[0])
 	if err != nil {
@@ -57,7 +56,7 @@ func checkArgumentsForElementalProcessAccount(args []string) (int, time.Time, er
 func taskElementalProcessAccount(ctx context.Context) error {
 	args := flag.Args()
 	logger := jsonlog.LoggerFromContextOrDefault(ctx)
-	logger.Debug("Running task 'process-account'.", map[string]interface{}{
+	logger.Debug("Running task 'elemental-process-account'.", map[string]interface{}{
 		"args": args,
 	})
 	aaId, date, err := checkArgumentsForElementalProcessAccount(args)
@@ -95,32 +94,17 @@ func ingestElementalDataForAccount(ctx context.Context, aaId int, date time.Time
 	}
 	if err != nil {
 		//updateAccountElementalProcessingCompletion(ctx, aaId, db.Db, updateId, err, nil, nil, nil, nil, nil, nil, nil, nil, nil, nil, false)
-		logger.Error("Failed to process account data.", map[string]interface{}{
+		logger.Error("Failed to process account elemental data.", map[string]interface{}{
 			"awsAccountId": aaId,
 			"error":        err.Error(),
 		})
 	}
-	var affectedRoutes = []string{
-		"/ec2",
-		"/ec2/coverage",
-		"/ec2/unused",
-		"/elasticache",
-		"/elasticache/unused",
-		"/es",
-		"/es/unused",
-		"/lambda",
-		"/rds",
-		"/rds/unused",
-		"/ri/ec2",
-		"/ri/rds",
-	}
-	_ = cache.RemoveMatchingCache(affectedRoutes, []string{aa.AwsIdentity}, logger)
 	return
 }
 
 func registerAccountElementalProcessing(db *sql.DB, aa aws.AwsAccount) (int64, error) {
-	const sqlstr = `INSERT INTO aws_account_update_job(
-		aws_account_id,
+	const sqlstr = `INSERT INTO aws_account_elemental_update_job(
+		aws_account_elemental_id,
 		worker_id
 	) VALUES (?, ?)`
 	res, err := db.Exec(sqlstr, aa.Id, backendId)
@@ -130,12 +114,12 @@ func registerAccountElementalProcessing(db *sql.DB, aa aws.AwsAccount) (int64, e
 	return res.LastInsertId()
 }
 
-func updateAccountElementalProcessingCompletion(ctx context.Context, aaId int, db *sql.DB, updateId int64, jobErr, rdsErr, ec2Err, esErr, elastiCacheErr, lambdaErr, riEc2Err, riRdsErr, odToRiEc2Err, historyErr, ebsErr error, historyCreated bool) {
+func updateAccountElementalProcessingCompletion(ctx context.Context, aaId int, db *sql.DB, updateId int64, jobErr, historyErr error, historyCreated bool) {
 	updateNextElementalUpdateAccount(db, aaId)
-	rErr := registerAccountElementalProcessingCompletion(db, updateId, jobErr, rdsErr, ec2Err, esErr, elastiCacheErr, lambdaErr, riEc2Err, riRdsErr, odToRiEc2Err, historyErr, ebsErr, historyCreated)
+	rErr := registerAccountElementalProcessingCompletion(db, updateId, jobErr, historyErr, historyCreated)
 	if rErr != nil {
 		logger := jsonlog.LoggerFromContextOrDefault(ctx)
-		logger.Error("Failed to register account processing completion.", map[string]interface{}{
+		logger.Error("Failed to register account processing elemental completion.", map[string]interface{}{
 			"awsAccountId": aaId,
 			"error":        rErr.Error(),
 			"updateId":     updateId,
@@ -145,29 +129,20 @@ func updateAccountElementalProcessingCompletion(ctx context.Context, aaId int, d
 
 func updateNextElementalUpdateAccount(db *sql.DB, aaId int) error {
 	const sqlstr = `UPDATE aws_account SET
-		next_update=?
+		next_elemental_update=?
 	WHERE id=?`
 	_, err := db.Exec(sqlstr, time.Now().AddDate(0, 0, 1), aaId)
 	return err
 }
 
-func registerAccountElementalProcessingCompletion(db *sql.DB, updateId int64, jobErr, rdsErr, ec2Err, esErr, elastiCacheErr, lambdaErr, riEc2Err, riRdsErr, odToRiEc2Err, historyErr, ebsErr error, historyCreated bool) error {
-	const sqlstr = `UPDATE aws_account_update_job SET
+func registerAccountElementalProcessingCompletion(db *sql.DB, updateId int64, jobErr, historyErr error, historyCreated bool) error {
+	const sqlstr = `UPDATE aws_account_elemental_update_job SET
 		completed=?,
 		jobError=?,
-		rdsError=?,
-		ec2Error=?,
-		esError=?,
-		elastiCacheError=?,
-		lambdaError=?,
-		ebsError=?,
-		riEc2Error=?,
-		riRdsError=?,
-		odToRiEc2Error=?,
 		historyError=?,
 		monthly_reports_generated=?
 	WHERE id=?`
-	_, err := db.Exec(sqlstr, time.Now(), errToStr(jobErr), errToStr(rdsErr), errToStr(ec2Err), errToStr(esErr), errToStr(elastiCacheErr), errToStr(lambdaErr), errToStr(ebsErr), errToStr(riEc2Err), errToStr(riRdsErr), errToStr(odToRiEc2Err), errToStr(historyErr), historyCreated, updateId)
+	_, err := db.Exec(sqlstr, time.Now(), errToStr(jobErr), errToStr(historyErr), historyCreated, updateId)
 	return err
 }
 
@@ -176,7 +151,7 @@ func elementalProcessAccountHistory(ctx context.Context, aa aws.AwsAccount, date
 	status, err := history.FetchHistoryInfos(ctx, aa, date)
 	if err != nil && err != history.ErrBillingDataIncomplete {
 		logger := jsonlog.LoggerFromContextOrDefault(ctx)
-		logger.Error("Failed to ingest History data.", map[string]interface{}{
+		logger.Error("Failed to ingest History elemental data.", map[string]interface{}{
 			"awsAccountId": aa.Id,
 			"error":        err.Error(),
 		})
