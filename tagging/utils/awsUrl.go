d48fb5a4e9ec22f32829813303493333417e3608
Alexandre Sauner <alexandre@trackit.io>
2020-07-01 15:44:45 +0200

    Fix: GetRegionForURL crashed when passed an empty string

--- tagging/utils/awsUrl.go
+++ tagging/utils/awsUrl.go
@@ -4,9 +4,14 @@ import "strconv"
 
 // GetRegionForURL removes the last character of an AWS region if it's not a number
 func GetRegionForURL(region string) string {
+	if len(region) <= 0 {
+		return region
+	}
+
 	_, err := strconv.ParseInt(region[len(region)-1:], 10, 32)
 	if err != nil {
 		region = region[:len(region)-1]
 	}
+
 	return region
 }
