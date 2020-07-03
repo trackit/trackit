b479f0f9d1cce86e64aaec9c327bfb945917524c
Alexandre Sauner <alexandre@trackit.io>
2020-07-03 14:12:52 +0200

    Added error logging in get most used tags route

--- tagging/routes/routeGetMostUsedTags.go
+++ tagging/routes/routeGetMostUsedTags.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/trackit/jsonlog"
+
 	"github.com/trackit/trackit/db"
 	"github.com/trackit/trackit/models"
 	"github.com/trackit/trackit/routes"
@@ -12,13 +14,14 @@ import (
 )
 
 func routeGetMostUsedTags(r *http.Request, a routes.Arguments) (int, interface{}) {
+	logger := jsonlog.LoggerFromContextOrDefault(r.Context())
 	u := a[users.AuthenticatedUser].(users.User)
-
 	dateBegin := a[mostUsedTagsQueryArgs[0]].(time.Time)
 	dateEnd := a[mostUsedTagsQueryArgs[1]].(time.Time).Add(time.Hour*time.Duration(23) + time.Minute*time.Duration(59) + time.Second*time.Duration(59))
 
 	dbRes, err := models.MostUsedTagsByAwsAccountIDInRange(db.Db, u.Id, dateBegin, dateEnd)
 	if err != nil {
+		logger.Error("Could not fetch most used tags.", err.Error())
 		return 500, nil
 	}
 
@@ -27,6 +30,7 @@ func routeGetMostUsedTags(r *http.Request, a routes.Arguments) (int, interface{}
 		tagsList := []string{}
 		err = json.Unmarshal([]byte(entry.Tags), &tagsList)
 		if err != nil {
+			logger.Error("Could not unmarshall most used tags.", err.Error())
 			continue
 		}
 
