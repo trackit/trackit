93ac77f18d5aacc54b1fd4af2b4f7134b82a7cd2
Alexandre Sauner <alexandre@trackit.io>
2020-07-03 09:27:28 +0200

    Improved ES query for most used tags (with ignored tags now)

--- tagging/updateMostUsedTags.go
+++ tagging/updateMostUsedTags.go
@@ -16,7 +16,11 @@ import (
 
 // UpdateMostUsedTagsForAccount updates most used tags in MySQL for the specified AWS account
 func UpdateMostUsedTagsForAccount(ctx context.Context, account int, awsAccount string) error {
-	mostUsedTags, err := getMostUsedTagsForAccount(ctx, account)
+	mostUsedTags, err := getMostUsedTagsForAccount(ctx, account, []string{
+		"aws:cloudformation:stack-id",
+		"aws:cloudformation:logical-id",
+		"aws:cloudformation:stack-name",
+	})
 	if err != nil {
 		return err
 	}
@@ -34,7 +38,7 @@ func UpdateMostUsedTagsForAccount(ctx context.Context, account int, awsAccount s
 	return model.Insert(db.Db)
 }
 
-func getMostUsedTagsForAccount(ctx context.Context, account int) ([]string, error) {
+func getMostUsedTagsForAccount(ctx context.Context, account int, ignoredTags []string) ([]string, error) {
 	client := es.Client
 	indexName := es.IndexNameForUserId(account, destIndexName)
 
@@ -46,11 +50,14 @@ func getMostUsedTagsForAccount(ctx context.Context, account int) ([]string, erro
 		return []string{}, nil
 	}
 
+	filterQueries := getFilterQueriesFromIgnoredTags(ignoredTags)
+
 	index := client.Search().Index(indexName)
-	topTags := elastic.NewTermsAggregation().Field("tags.key").Size(5)
-	tags := elastic.NewNestedAggregation().Path("tags").SubAggregation("topTags", topTags)
-	reportDate := elastic.NewTermsAggregation().Field("reportDate").Order("_term", false).Size(1).SubAggregation("tags", tags)
-	res, err := index.Size(0).Query(elastic.NewMatchAllQuery()).Aggregation("reportDate", reportDate).Do(ctx)
+	termsAgg := elastic.NewTermsAggregation().Field("tags.key").Size(5)
+	filterAgg := elastic.NewFilterAggregation().Filter(elastic.NewBoolQuery().MustNot(filterQueries...)).SubAggregation("terms", termsAgg)
+	nestedAgg := elastic.NewNestedAggregation().Path("tags").SubAggregation("filter", filterAgg)
+	reportDateAgg := elastic.NewTermsAggregation().Field("reportDate").Order("_term", false).Size(1).SubAggregation("nested", nestedAgg)
+	res, err := index.Size(0).Query(elastic.NewMatchAllQuery()).Aggregation("reportDate", reportDateAgg).Do(ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -58,23 +65,37 @@ func getMostUsedTagsForAccount(ctx context.Context, account int) ([]string, erro
 	return processMostUsedTagsResult(res)
 }
 
+func getFilterQueriesFromIgnoredTags(ignoredTags []string) []elastic.Query {
+	queries := []elastic.Query{}
+
+	for _, ignoredTag := range ignoredTags {
+		queries = append(queries, elastic.NewTermQuery("tags.key", ignoredTag))
+	}
+
+	return queries
+}
+
 func processMostUsedTagsResult(res *elastic.SearchResult) ([]string, error) {
 	reportDateRes, found := res.Aggregations.Terms("reportDate")
 	if !found || len(reportDateRes.Buckets) <= 0 {
 		return nil, errors.New("could not query elastic search")
 	}
-	tagsRes, found := reportDateRes.Buckets[0].Aggregations.Nested("tags")
+	nestedRes, found := reportDateRes.Buckets[0].Aggregations.Nested("nested")
 	if !found {
 		return nil, errors.New("could not query elastic search")
 	}
-	topTagsRes, found := tagsRes.Aggregations.Terms("topTags")
+	filterRes, found := nestedRes.Aggregations.Filter("filter")
+	if !found {
+		return nil, errors.New("could not query elastic search")
+	}
+	termsRes, found := filterRes.Aggregations.Terms("terms")
 	if !found {
 		return nil, errors.New("could not query elastic search")
 	}
 
 	mostUsedTags := []string{}
 
-	for _, result := range topTagsRes.Buckets {
+	for _, result := range termsRes.Buckets {
 		mostUsedTags = append(mostUsedTags, fmt.Sprintf("%s", result.Key))
 	}
 
