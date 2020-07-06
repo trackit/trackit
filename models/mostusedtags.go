696ecaac6df2b1af52ba6d70718b9f4b2f8d097d
Alexandre Sauner <alexandre@trackit.io>
2020-07-06 09:44:49 +0200

    Added a function to returns latest most used tags from DB

--- models/mostusedtags.go
+++ models/mostusedtags.go
@@ -42,3 +42,45 @@ func MostUsedTagsByAwsAccountIDInRange(db XODB, awsAccountID int, begin time.Tim
 
 	return res, nil
 }
+
+// LatestMostUsedTagsByAwsAccountID returns the latest most used tags of an AWS account.
+func LatestMostUsedTagsByAwsAccountID(db XODB, awsAccountID int, begin time.Time, end time.Time) (*MostUsedTag, error) {
+	var err error
+
+	// sql query
+	sqlstr := `SELECT ` +
+		`id, report_date, aws_account_id, tags ` +
+		`FROM trackit.most_used_tags ` +
+		`WHERE aws_account_id = ? ` +
+		`ORDER BY report_date DESC LIMIT 1`
+
+	// run query
+	XOLog(sqlstr, awsAccountID)
+	q, err := db.Query(sqlstr, awsAccountID)
+	if err != nil {
+		return nil, err
+	}
+	defer q.Close()
+
+	// load results
+	res := []*MostUsedTag{}
+	for q.Next() {
+		mut := MostUsedTag{
+			_exists: true,
+		}
+
+		// scan
+		err = q.Scan(&mut.ID, &mut.ReportDate, &mut.AwsAccountID, &mut.Tags)
+		if err != nil {
+			return nil, err
+		}
+
+		res = append(res, &mut)
+	}
+
+	if len(res) <= 0 {
+		return nil, nil
+	}
+
+	return res[0], nil
+}
