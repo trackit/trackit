875386a11d38e83d066e6c70b8a29a5ba4a2f6fb
Alexandre Sauner <alexandre@trackit.io>
2020-07-23 14:12:51 +0200

    Prevent reminder mails to send negative time before deletion

--- unusedAccounts/sendRemainder.go
+++ unusedAccounts/sendRemainder.go
@@ -11,6 +11,10 @@ import (
 
 func sendRemainder(ctx context.Context, user models.User, timeBeforeDeletion time.Duration) error {
 	daysBeforeDeletion := timeBeforeDeletion / (time.Hour * 24)
+	if daysBeforeDeletion < 1 {
+		daysBeforeDeletion = 1
+	}
+
 	body := fmt.Sprintf("Your TrackIt account is not used anymore. Please login again or your data will be deleted in %d days.", daysBeforeDeletion)
 	return mail.SendMail(user.Email, "Your TrackIt account is not used anymore.", body, ctx)
 }
