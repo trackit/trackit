45aff3fd57151fb0d73ab78311d75ae9e82a17b6
Nicolas Schneider <nicolas.schneider@epitech.eu>
2020-07-23 16:18:34 +0200

    Changed route to get resources

--- tagging/routes/routeGetResources.go
+++ tagging/routes/routeGetResources.go
@@ -16,16 +16,13 @@ package routes
 
 import (
 	"context"
-	"database/sql"
 	"errors"
 	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/olivere/elastic"
 	"github.com/trackit/jsonlog"
 
-	"github.com/trackit/trackit/db"
 	"github.com/trackit/trackit/routes"
 	"github.com/trackit/trackit/users"
 	terrors "github.com/trackit/trackit/errors"
@@ -34,35 +31,25 @@ import (
 	"github.com/trackit/trackit/tagging"
 )
 
-type (
-	// ResourcesQueryParams will store the parsed query params
-	ResourcesQueryParams struct {
-		AccountsList   []string
-		IndexesList    []string
-		Regions        []string
-		ResourceTypes  []string
-	}
-)
+type Tag struct {
+	Key   string `json:"key"   req:"nonzero"`
+	Value string `json:"value" req:"nonzero"`
+}
 
-// routeGetResources returns the list of resources based on the query params, in JSON format.
+type ResourcesRequestBody struct {
+	Accounts      []string `json:"accounts"      req:"nonzero"`
+	Regions       []string `json:"regions"       req:"nonzero"`
+	ResourceTypes []string `json:"resourceTypes" req:"nonzero"`
+	Tags          []Tag    `json:"tags"          req:"nonzero"`
+	MissingTags   []Tag    `json:"missingTags"   req:"nonzero"`
+}
+
+// routeGetResources returns the list of resources based on the request body, in JSON format.
 func routeGetResources(request *http.Request, a routes.Arguments) (int, interface{}) {
+	var body ResourcesRequestBody
+	routes.MustRequestBody(a, &body)
 	user := a[users.AuthenticatedUser].(users.User)
-	tx := a[db.Transaction].(*sql.Tx)
-	parsedParams := ResourcesQueryParams{
-		AccountsList:  []string{},
-		Regions:       []string{},
-		ResourceTypes: []string{},
-	}
-	if a[routes.AwsAccountsOptionalQueryArg] != nil {
-		parsedParams.AccountsList = a[routes.AwsAccountsOptionalQueryArg].([]string)
-	}
-	if a[resourcesQueryArgs[1]] != nil {
-		parsedParams.Regions = a[resourcesQueryArgs[1]].([]string)
-	}
-	if a[resourcesQueryArgs[2]] != nil {
-		parsedParams.ResourceTypes = a[resourcesQueryArgs[2]].([]string)
-	}
-	returnCode, report, err := GetResourcesData(request.Context(), parsedParams, user, tx)
+	returnCode, report, err := GetResources(request.Context(), body, user)
 	if err != nil {
 		return returnCode, err
 	} else {
@@ -70,24 +57,9 @@ func routeGetResources(request *http.Request, a routes.Arguments) (int, interfac
 	}
 }
 
-// GetResourcesData gets resources report based on query params
-func GetResourcesData(ctx context.Context, parsedParams ResourcesQueryParams, user users.User, tx *sql.Tx) (int, []utils.TaggingReportDocument, error) {
-	accountsAndIndexes, returnCode, err := es.GetAccountsAndIndexes(parsedParams.AccountsList, user, tx, tagging.IndexPrefixTaggingReport)
-	if err != nil {
-		return returnCode, nil, err
-	}
-	parsedParams.AccountsList = accountsAndIndexes.Accounts
-	parsedParams.IndexesList = accountsAndIndexes.Indexes
-	returnCode, resources, err := GetResources(ctx, parsedParams)
-	if err != nil {
-		return returnCode, nil, err
-	}
-	return returnCode, resources, nil
-}
-
-// GetResources does an elastic request and returns an array of resources report based on query params
-func GetResources(ctx context.Context, params ResourcesQueryParams) (int, []utils.TaggingReportDocument, error) {
-	res, returnCode, err := makeElasticSearchRequest(ctx, params, getElasticSeachResourcesParams)
+// GetResources does an elastic request and returns an array of resources report based on the request body
+func GetResources(ctx context.Context, params ResourcesRequestBody, user users.User) (int, []utils.TaggingReportDocument, error) {
+	res, returnCode, err := makeElasticSearchRequest(ctx, params, user, getElasticSeachResourcesParams)
 	if err != nil {
 		return returnCode, nil, err
 	} else if res == nil {
@@ -101,38 +73,38 @@ func GetResources(ctx context.Context, params ResourcesQueryParams) (int, []util
 }
 
 // makeElasticSearchRequest prepares and run an ES request
-// based on the ResourcesQueryParams and search params
+// based on the ResourcesRequestBody and search params
 // It will return the data, an http status code (as int) and an error.
 // Because an error can be generated, but is not critical and is not needed to be known by
 // the user (e.g if the index does not exists because it was not yet indexed ) the error will
 // be returned, but instead of having a 500 status code, it will return the provided status code
 // with empty data
-func makeElasticSearchRequest(ctx context.Context, parsedParams ResourcesQueryParams,
-	esSearchParams func(ResourcesQueryParams, *elastic.Client, string) *elastic.SearchService) (*elastic.SearchResult, int, error) {
-	l := jsonlog.LoggerFromContextOrDefault(ctx)
-	index := strings.Join(parsedParams.IndexesList, ",")
-	searchService := esSearchParams(
-		parsedParams,
-		es.Client,
-		index,
-	)
-	res, err := searchService.Do(ctx)
-	if err != nil {
-		if elastic.IsNotFound(err) {
-			l.Warning("Query execution failed, ES index does not exists", map[string]interface{}{
-				"index": index,
-				"error": err.Error(),
-			})
-			return nil, http.StatusOK, terrors.GetErrorMessage(ctx, err)
-		} else if cast, ok := err.(*elastic.Error); ok && cast.Details != nil && cast.Details.Type == "search_phase_execution_exception" {
-			l.Error("Error while getting data from ES", map[string]interface{}{
-				"type":  fmt.Sprintf("%T", err),
-				"error": err,
-			})
-		} else {
-			l.Error("Query execution failed", map[string]interface{}{"error": err.Error()})
+func makeElasticSearchRequest(ctx context.Context, params ResourcesRequestBody, user users.User,
+	esSearchParams func(ResourcesRequestBody, *elastic.Client, string) *elastic.SearchService) (*elastic.SearchResult, int, error) {
+		l := jsonlog.LoggerFromContextOrDefault(ctx)
+		index := es.IndexNameForUser(user, tagging.IndexPrefixTaggingReport)
+		searchService := esSearchParams(
+			params,
+			es.Client,
+			index,
+		)
+		res, err := searchService.Do(ctx)
+		if err != nil {
+			if elastic.IsNotFound(err) {
+				l.Warning("Query execution failed, ES index does not exists", map[string]interface{}{
+					"index": index,
+					"error": err.Error(),
+				})
+				return nil, http.StatusOK, terrors.GetErrorMessage(ctx, err)
+			} else if cast, ok := err.(*elastic.Error); ok && cast.Details != nil && cast.Details.Type == "search_phase_execution_exception" {
+				l.Error("Error while getting data from ES", map[string]interface{}{
+					"type":  fmt.Sprintf("%T", err),
+					"error": err,
+				})
+			} else {
+				l.Error("Query execution failed", map[string]interface{}{"error": err.Error()})
+			}
+			return nil, http.StatusInternalServerError, terrors.GetErrorMessage(ctx, err)
 		}
-		return nil, http.StatusInternalServerError, terrors.GetErrorMessage(ctx, err)
-	}
-	return res, http.StatusOK, nil
+		return res, http.StatusOK, nil
 }
