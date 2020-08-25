9f9a775de7ebfe8d0788981803c211e5c535b857
Alexandre Sauner <alexandre@trackit.io>
2020-08-25 18:34:45 +0200

    Moved wrongly placed comment

--- costs/es_request_constructor.go
+++ costs/es_request_constructor.go
@@ -265,14 +265,14 @@ func nestAggregation(allAggrSlice []paramAggrAndName) elastic.Aggregation {
 //	- If a param in the slice is not present in the detailedLineItemsFieldsName, the program will crash.
 //	- If the client is nil or malconfigured, it will crash
 //	- If the index is not an index present in the ES, it will crash
+// We are excluding AWSDataTransfer products because it's value is always zero.
+// Data transfer costs are included in other products' costs.
 func GetElasticSearchParams(accountList []string, durationBegin time.Time,
 	durationEnd time.Time, params []string, client *elastic.Client, index string) *elastic.SearchService {
 	query := elastic.NewBoolQuery()
 	if len(accountList) > 0 {
 		query = query.Filter(createQueryAccountFilter(accountList))
 	}
-	// We are excluding AWSDataTransfer products because it's value is always zero.
-	// Data transfer costs are included in other products' costs.
 	query = query.Filter(createQueryTimeRange(durationBegin, durationEnd),
 		elastic.NewBoolQuery().MustNot(elastic.NewTermQuery("productCode", "AWSDataTransfer")))
 	search := client.Search().Index(index).Size(0).Query(query)
