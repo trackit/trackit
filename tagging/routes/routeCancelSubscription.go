d5e9e9adc5e4ae348ac20280f43ab170b130f6c3
Nicolas Schneider <nicolas.schneider@epitech.eu>
2020-09-22 13:46:11 +0200

    Added route to cancel subscription

--- tagging/routes/routeCancelSubscription.go
+++ tagging/routes/routeCancelSubscription.go
@@ -19,11 +19,11 @@ import (
 	"database/sql"
     "errors"
 
-    "github.com/stripe/stripe-go"
-	"github.com/stripe/stripe-go/sub"
-
+	"github.com/stripe/stripe-go/v72"
+	"github.com/stripe/stripe-go/v72/sub"
 	"github.com/trackit/jsonlog"
 
+	"github.com/trackit/trackit/config"
 	"github.com/trackit/trackit/db"
 	"github.com/trackit/trackit/models"
     "github.com/trackit/trackit/routes"
@@ -45,8 +45,8 @@ func routeCancelSubscription(request *http.Request, a routes.Arguments) (int, in
 			"error":  err.Error(),
 		})
 		return http.StatusInternalServerError, errors.New("Failed to get Tagbot user with id")
-    }
-	stripe.Key = "sk_test_51HAGBpHPvmk5HTchHxZJ0h9RGC1M8DAVuoSQeQJc3fbLFXII39hjAEMMxp0A7sAm5leXcZe8qsKbxHUXfBefjySO00ZJ4TSYeP"
+	}
+	stripe.Key = config.StripeKey
 	var body CancelSubscriptionRequestBody
 	routes.MustRequestBody(a, &body)
 	s, err := sub.Cancel(body.SubscriptionID, nil)
